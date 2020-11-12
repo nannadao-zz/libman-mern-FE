c35e81186852c4857a047b740cab2ba45632f0bd
Nanna Dao <daohoangngan1997@gmail.com>
2020-11-12 19:49:01 +0200

    add: dockerfile

--- src/components/Login Card/login.tsx
+++ src/components/Login Card/login.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
